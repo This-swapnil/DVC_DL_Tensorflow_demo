346dd1fcd11451a59fc25c213cde488380c7d455
This-swapnil <sswapnil0098@gmail.com>
2022-03-31 11:19:58 +0530

    stage_01 update

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 def copy_file(source_download_path, local_data_dir):
     list_of_file = os.listdir(source_download_path)
     N = len(list_of_file)
-    for file in tqdm(list_of_file,total=N,desc= f"copying fiel from {source_download_path} to {local_data_dir}",colour="green"):
+    for file in tqdm(list_of_file,total=N,desc= f"copying file from {source_download_path} to {local_data_dir}",colour="green"):
         src = os.path.join(source_download_path, file)
         dest = os.path.join(local_data_dir, file)
         shutil.copy(src, dest)
@@ -17,7 +17,7 @@ def get_data(config_path):
     config = read_yaml(config_path)
 
     source_download_dirs = config['source_download_dirs']
-    local_data_dir = config['local_data_path']
+    local_data_dir = config['local_data_dir']
 
     for source_download_path,local_data_dir in tqdm(zip(source_download_dirs,local_data_dir),total=2,desc= "list of folders",colour="red"):
         create_dir([local_data_dir])
